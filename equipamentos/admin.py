5c1ea47e0844a700a901392dfd62248a137f67d2
Guilherme Canedo <guilhermecanedo09@gmail.com>
2025-05-15 15:43:22 -0300

    Adicionando meu arquivo admin.py

--- equipamentos/admin.py
+++ equipamentos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Departamento, Sala, Responsavel, Equipamento
 
-# Register your models here.
+admin.site.register(Departamento)
+admin.site.register(Sala)
+admin.site.register(Responsavel)
+admin.site.register(Equipamento)
