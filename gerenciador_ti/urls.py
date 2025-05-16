168821c99548ba034cf8884853e52f0079ec96fb
Guilherme Canedo <guilhermecanedo09@gmail.com>
2025-05-16 14:15:13 -0300

    Criando Templates

--- gerenciador_ti/urls.py
+++ gerenciador_ti/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('equipamentos/', include('equipamentos.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
+    path('equipamentos/', include('equipamentos.urls')),  # já está aí
+    path('', include('equipamentos.urls')),  # <-- adiciona essa linha!
 ]
